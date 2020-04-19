c45a05cc7b84a0e331432cd31f1d0342b2315a89
carlosd-barros <barros.carlosds@gmail.com>
2020-04-19 16:10:59 -0300

    full funcoinal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Class } from './models/Class';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Class } from './models/Class';
+import { Person } from './models/Person';
 
 @Component({
   selector: 'app-root',
@@ -8,35 +10,81 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BootstrapFormsAndValidation';
-  uf = ["AC","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"]
-  turmas = [
+  uf: Array<string> = ["AC","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS",
+  "MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
+  turmas: Array<Class> = [
     new Class("LPWEB","0720"),
     new Class("IA","0420"),
     new Class("SI","0219"),
     new Class("TS","0121")
-  ]
-  people = []
-  name;
-  birth;
-  cpf;
-  email;
-  phone;
-  endereco;
-  estado;
-  cep;
-  cidade;
-  turma;
-  documentos=false;
+  ];
+  students: Array<Person> = [];
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(70),
+      ])],
+      birth: ['', Validators.required],
+      sex: ['', Validators.required],
+      cpf: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(14),
+        Validators.maxLength(14),
+      ])],
+      email: ['', Validators.compose([
+        Validators.email,
+        Validators.required,
+      ])],
+      phone: ['', Validators.required],
+      address: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(30),
+      ])],
+      cep: ['', Validators.required],
+      uf: ['', Validators.required],
+      city: ['', Validators.required],
+      turmas: [{value: '', disabled: true}, Validators.required],
+      documentsValid: [false, Validators.required]
+    });
+  }
 
   save(){
-    console.log(this.documentos);
-  }
-  checkboxTrue(e){
-    this.documentos=e.target.checked;
-  
+    if (this.form.invalid) return;
+    const { name, birth, sex, cpf, email, phone, address, cep, uf, city, turmas, documentsValid } = this.form.value;
+    this.students.push(
+      new Person(name, birth, sex, cpf, email, phone, address, cep, uf, city, turmas, documentsValid)
+    );
   }
 
+  onReset() {
+    this.form.reset({
+      documentsValid: false,
+    });
+  }
 
+  public calcAge(date: string) {
+    const [y, m, d] = date.split('-');
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - parseInt(y);
+    if (currentDate.getMonth() >= parseInt(m)) return age;
+    return age -1;
+  }
+
+  public enableOrDisable() {
+    const fieldTurma = this.form.controls["turmas"];
+
+    if (!this.form.controls["documentsValid"].value) {
+      fieldTurma.enable();
+      return;
+    }
+    fieldTurma.disable()
+  }
 }
--- src/app/models/Person.ts
+++ src/app/models/Person.ts
@@ -1,6 +1,6 @@
 import { Class } from "./Class";
 
-export class Person{
+export class Person {
     name: string;
     birth: Date;
     sex: string;
@@ -11,6 +11,34 @@ export class Person{
     cep: number;
     uf: string;
     city: string;
-    class: Array<Class>;
+    turmas: Array<Class>;
     documentsValid: boolean;
+
+    constructor(
+        name: string,
+        birth: Date,
+        sex: string,
+        cpf: number,
+        email: string,
+        phone: number,
+        address: string,
+        cep: number,
+        uf: string,
+        city: string,
+        turmas: Array<Class>,
+        documentsValid: boolean = false
+    ){
+        this.name = name;
+        this.birth = birth;
+        this.sex = sex;
+        this.cpf = cpf;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.cep = cep;
+        this.uf = uf;
+        this.city = city;
+        this.turmas = turmas;
+        this.documentsValid = documentsValid;
+    }
 }
